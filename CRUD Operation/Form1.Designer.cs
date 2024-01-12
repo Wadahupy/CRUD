7636b9feaa4c457fae4e1f8b9911d943ff579d14
Denzkieeeeee <116092662+Wadahupy@users.noreply.github.com>
2024-01-12 22:36:05 +0800

    CRUD

---
+++ CRUD Operation/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace CRUD_Operation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblName = new System.Windows.Forms.Label();
+            this.LblAge = new System.Windows.Forms.Label();
+            this.LblGender = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtAge = new System.Windows.Forms.TextBox();
+            this.TxtGender = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblSearchRecords = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Location = new System.Drawing.Point(21, 130);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(46, 17);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "NAME";
+            // 
+            // LblAge
+            // 
+            this.LblAge.AutoSize = true;
+            this.LblAge.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAge.Location = new System.Drawing.Point(21, 184);
+            this.LblAge.Name = "LblAge";
+            this.LblAge.Size = new System.Drawing.Size(33, 17);
+            this.LblAge.TabIndex = 1;
+            this.LblAge.Text = "AGE";
+            // 
+            // LblGender
+            // 
+            this.LblGender.AutoSize = true;
+            this.LblGender.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGender.Location = new System.Drawing.Point(21, 239);
+            this.LblGender.Name = "LblGender";
+            this.LblGender.Size = new System.Drawing.Size(59, 17);
+            this.LblGender.TabIndex = 2;
+            this.LblGender.Text = "GENDER";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtName.Location = new System.Drawing.Point(24, 150);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(286, 22);
+            this.TxtName.TabIndex = 3;
+            // 
+            // TxtAge
+            // 
+            this.TxtAge.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtAge.Location = new System.Drawing.Point(24, 204);
+            this.TxtAge.Name = "TxtAge";
+            this.TxtAge.Size = new System.Drawing.Size(286, 22);
+            this.TxtAge.TabIndex = 4;
+            // 
+            // TxtGender
+            // 
+            this.TxtGender.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtGender.Location = new System.Drawing.Point(24, 259);
+            this.TxtGender.Name = "TxtGender";
+            this.TxtGender.Size = new System.Drawing.Size(286, 22);
+            this.TxtGender.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(411, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(377, 325);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "#";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 39;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "NAME";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "AGE";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 54;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "GENDER";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 78;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(323, 144);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 31);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(323, 196);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 35);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(323, 249);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 39);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(411, 60);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(286, 22);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(703, 52);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 32);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // lblSearchRecords
+            // 
+            this.lblSearchRecords.AutoSize = true;
+            this.lblSearchRecords.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchRecords.Location = new System.Drawing.Point(408, 40);
+            this.lblSearchRecords.Name = "lblSearchRecords";
+            this.lblSearchRecords.Size = new System.Drawing.Size(119, 17);
+            this.lblSearchRecords.TabIndex = 12;
+            this.lblSearchRecords.Text = "SEARCH RECORDS";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(24, 285);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 28);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 50);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "CRUD OPERATION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblSearchRecords);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TxtGender);
+            this.Controls.Add(this.TxtAge);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.LblGender);
+            this.Controls.Add(this.LblAge);
+            this.Controls.Add(this.LblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD OPERATION";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Label LblAge;
+        private System.Windows.Forms.Label LblGender;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtAge;
+        private System.Windows.Forms.TextBox TxtGender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblSearchRecords;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CRUD Operation/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace CRUD_Operation
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        int i = 0;
+        dbconnection dbconn = new dbconnection();
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(dbconn.dbconnect());
+            LoadRecord();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadRecord();
+        }
+
+        private void LoadRecord()
+        {
+            dataGridView1.Rows.Clear();
+            conn.Open();
+            cmd = new MySqlCommand("SELECT `Name`, `Age`, `Gender` FROM `crud`", conn);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add(dataGridView1.Rows.Count + 1, dr["Name"].ToString(), dr["Age"].ToString(), dr["Gender"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+
+        public void clear() 
+        {
+            TxtName.Clear();
+            TxtAge.Clear();
+            TxtGender.Clear();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if ((TxtName.Text == string.Empty) || (TxtAge.Text == string.Empty) || (TxtGender.Text == string.Empty))
+            {
+                MessageBox.Show("WARNING: Please Fill out the Field", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                conn.Open();
+                cmd = new MySqlCommand("INSERT INTO `crud`(`Name`, `Age`, `Gender`) VALUES (@Name, @Age, @Gender)", conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@Name", TxtName.Text);
+                cmd.Parameters.AddWithValue("@Age", TxtAge.Text);
+                cmd.Parameters.AddWithValue("@Gender", TxtGender.Text);
+
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Record Successfully Added!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to Add Records!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+                conn.Close();
+                LoadRecord();
+                clear();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new MySqlCommand("UPDATE `crud` SET `Name` = @NewName, `Age` = @Age, `Gender` = @Gender WHERE `Name` = @OldName", conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@NewName", TxtName.Text);
+            cmd.Parameters.AddWithValue("@Age", TxtAge.Text);
+            cmd.Parameters.AddWithValue("@Gender", TxtGender.Text);
+            cmd.Parameters.AddWithValue("@OldName", TxtName.Text); // Assuming 'Name' is the primary key
+
+            i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Record Successfully Updated!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed to Update Records!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            conn.Close();
+            LoadRecord();
+            clear();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new MySqlCommand("DELETE FROM `crud` WHERE `Name` = @Name", conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Name", TxtName.Text); // Assuming 'Name' is the primary key
+
+            i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Record Successfully Deleted!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed to Delete Record!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            conn.Close();
+            LoadRecord();
+            clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TxtName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            TxtAge.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            TxtGender.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            conn.Open();
+            cmd = new MySqlCommand("SELECT `Name`, `Age`, `Gender` FROM `crud` WHERE `Name` LIKE @Search OR `Age` LIKE @Search OR `Gender` LIKE @Search OR `Gender` LIKE @Search ", conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Search", "%" + txtSearch.Text + "%");
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add(dataGridView1.Rows.Count + 1, dr["Name"].ToString(), dr["Age"].ToString(), dr["Gender"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
